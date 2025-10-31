c06362a7310ec9e5bdff0e1abaf48fa43f067e24
sangram behera <sangrambeheraa007@gmail.com>
2025-10-31 09:36:29 +0530

    Resolve merge conflicts and integrate remote changes

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import  'dotenv/config';
-import { connectDB } from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
-import path from 'path'
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import resumeRoutes from './routes/resumeRoutes.js';
-
+import resumeRoutes from "./routes/resumeRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT =4000;
+const PORT = 4000;
 
 app.use(cors());
-
-
 
 //connect to database
 connectDB();
@@ -26,24 +23,23 @@ connectDB();
 //middlewares
 app.use(express.json());
 
-app.use('/api/auth', userRoutes)
-app.use('/api/resume',resumeRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/resume", resumeRoutes);
 app.use(
-    '/uploads',
-    express.static(path.join(__dirname,'/uploads'),{
-    setHeaders:(res, _path) =>{
-        res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
-
-    }
-}))
+  "/uploads",
+  express.static(path.join(__dirname, "/uploads"), {
+    setHeaders: (res, _path) => {
+      res.set("Access-Control-Allow-Origin", "http://localhost:5173","https://resume-builder-rway.vercel.app/");
+    },
+  })
+);
 
 //routes
 
-app.get('/', (req, res) => {
-    res.send('API  WORKING')
+app.get("/", (req, res) => {
+  res.send("API  WORKING");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
